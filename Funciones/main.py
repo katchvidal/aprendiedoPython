4d8a3738b034bb1b543b51563bb546d0d7d82d4b
Katch <katchvidal@gmail.com>
2022-01-31 19:40:01 -0600

    function: learning about function

---
+++ Funciones/main.py
@@ -0,0 +1,12 @@
+#   Funciones en Python
+"""
+Funcion: conjunto de instrucciones agrupada bajo un nombre en concreto que puede reutilizarse tantas veces como sea necesario
+Palabra Reservada: def
+"""
+
+#   Primer Hello World
+def Hola():
+    name = input("Introduce tu nombre: ")
+    return print(f"Hola {name} desde tu primera funcion")
+
+Hola() #    Puedes invocar una funcion tantas veces sean necesarias
